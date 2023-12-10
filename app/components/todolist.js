df13b05149658b05e6096c0eb74adab146afd0e9
Qianying Ruan <124208512+1239874569898@users.noreply.github.com>
2023-12-10 03:21:30 -0800

    Update todolist.js

---
+++ app/components/todolist.js
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState } from "react";
+
+export default function ToDoList() {
+    const [topic, setTopic] = useState("");
+    const [time, setTime] = useState("");
+    const [Content, setContent] = useState("");
+
+    const ToDoList = {
+        topic,
+        time,
+        content,
+      };
+
+      
+    setTopic("");
+    setTime("");
+    setContent("");
+
+
+    return
+    <main>
+        <div></div>
+    </main>
