d36fe028629da14f983454b66090a91a7235819a
Qianying Ruan <124208512+1239874569898@users.noreply.github.com>
2023-12-10 03:20:35 -0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="text-4xl font-bold m-6 text-center text-blue-200">
       <div className="text-center my-7">
         <h1 className="text-2xl font-bold">My Days</h1>
+        <ToDoList />
       </div>
     </main>
 
